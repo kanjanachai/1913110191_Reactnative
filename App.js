1bc7d0e6386ea55663b945a64e9d78eaccf5e10f
kanjanachai <lin.14525@gmail.com>
2022-11-16 13:46:36 +0700

    "Basic Component"

--- App.js
+++ App.js
@@ -3,12 +3,16 @@ import React from 'react'
 import Information from './components/Information'
 import Welcome from './components/Welcome'
 import Practice1_StyleSheet from './components/Practice1_StyleSheet'
+import TxtInput from './components/TxtInput'
+import CustomComponent from './components/CustomComponent'
 const app = () => {
   return (
     <View>
       {/*<Information/>*/}
       {/*<Welcome/>*/}
-      <Practice1_StyleSheet/>
+      {/*<Practice1_StyleSheet/>*/}
+      {/*<TxtInput/>*/}
+      <CustomComponent/>
     </View>
   )
 }
